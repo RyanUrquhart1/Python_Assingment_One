0af6eaf1fbb169fe8bfaa55ac2a977929ddb3294
RyanUrquhart1 <32197736+RyanUrquhart1@users.noreply.github.com>
2017-12-11 14:38:45 +0000

    commenting

--- agentframework1.py
+++ agentframework1.py
@@ -17,7 +17,8 @@ Ryan Urquhart - Assingment One - GEOG5995
 
 """
 
-#This File contains 
+#This File contains my agent class for the practical work.
+#The code in this file defines what an agent is and how they interact as individuals, with each other and with the environment.
 
 class Agent():
     def __init__ (self, environment, agents):
@@ -26,6 +27,11 @@ class Agent():
         self.store = 0
         self.x = random.randint(0,100)
         self.y = random.randint(0,100)
+
+#The code above defines the agent and places this in the environment.
+#When imported to the model code it this dicates how the agents behave.
+#Within this code we have assinged each agent a store of zero, this will increase as they interacted with the environment.
+#Each Agent will start with a random x/y cordinate on a 100x100 grid.
 
     def move(self):
         if random.random() < 0.5:
@@ -37,17 +43,30 @@ class Agent():
         else:
             self.y = (self.y - 1) % 99
             
+#The above 'move' code is the movement of the agents.
+#Using the random function the cordinates will change based on a generated figure, allowing movements to be classed as 'random'.
+          
+            
     def eat(self):
         if self.environment[self.y][self.x] > 10:
                self.environment[self.y][self.x] -=10
                self.store +=10
+        
+#The above 'eat' code is the interaction with the environment. The store variable created earlier is used.
+#If the point the agents are stood on has an environment figure greater than 10, this will be taken off the environment and added to the agent store variable.
+#The environemnt value is defined by the csv file read in the model programme with each number making up the environment.        
+
 
     def vom(self):
         if self.store > 1000:
             self.store = 750
             self.environment[self.x][self.y] = self.environment[self.x][self.y] + 250
-       
-       
+  
+ #The above 'vom' code is further interaction with the environment.
+#If the agents store variable passes a certain level they will return a value back to the environment file.
+#Once this happens their store value goes back to 750 out of a possible 1000, meaning they don't return ALL their store, just a quarter.
+    
+    
     def share_with_neighbours(self, neighbourhood):
         for agent in self.agents:
             dist = self.distance_between(agent)
@@ -57,7 +76,12 @@ class Agent():
                 self.store = ave
                 agent.store = ave 
                 #print("sharing" + str(dist) + " " + str(ave))
-                
+     
+#The 'share_with_mneighbours' is the point communication between agents enters the model.
+#Here agents are either adjusting their variables or each others and uses the distance inbetween individual agents that is below this comment.
+#The commented out print command is to check the code is working and can indlued to return distances between agents.
+       
+           
     def distance_between(self, agent):
         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
         
--- practicemodel.py
+++ practicemodel.py
@@ -15,57 +15,72 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-
+#functions used within the code are imported at the start of the code.
+#The first three are built in functions where as 'agentframework1' contains the agent class that I have created.
 
 import random
 import operator
 import matplotlib.pyplot
 import agentframework1      
   
-
-
+#Distance Between agents is cacluted and set to return the distance figure. 
+#This code is used the agent to agent communication later on.
       
 def distance_between(agent0, agent1):
     return (((agent0.x - agent1.x)**2) + ((agent0.y - agent1.y)**2))**0.5  
 
 
-
-      
+#The following code sets up the basics of the model. We have empty lists for the environement and the agents that will be filled later in the code.
+#I have set the value of agents to be 15 with the modell running 200 times. The neighborhood value is used in agent to agent communication.     
 environment = []        
 num_of_agents = 15
 num_of_iterations = 200
 neighbourhood = 5
-
 agents = []
 
+
+#The data is read into the model in CSV format, this file contains the environemnt information.
+#The numbers represent the amount of food at any given point, the higher the number, the more food and the darker the colour of the visual output.
 import csv
 f = open ('in.txt', newline='')
 reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)        
  
-
-
+#The environment data is then stored in the blank environment list created a few steps previously.
 
 for row in reader:
     rowlist = []
     for item in row:
        rowlist.append(item)
     environment.append(rowlist)
-    
 f.close()
+
+#The matplotlib code below is to ensure the environment data is working correctly. 
+#This creates the first of three visual outputs, the inital environemnt before any agents appearence or interaction.
 
 matplotlib.pyplot.imshow(environment)
 matplotlib.pyplot.show()
 
     
+#We then create the agents. This is done using the code created earlier and using the imported agentframework1 code.
+#This creates 15 individual agents and stores them in the empty agents list.
+#The agents position on the grid, characteristics and behaviour is dictated by the agentfamework agent class.
 
-  
-# Make the agents.
 for i in range(num_of_agents):
     agents.append(agentframework1.Agent(environment, agents))        
-     
-    
-    
-# Move the agents.
+
+#I have then added new matplotlib code to show where the agents start on the environemnt.
+#We can see looking at the second visual output they have already taken values from the environment and added to their 'store'.     
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.imshow(environment)
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
+matplotlib.pyplot.show()
+   
+# Moving the agents uses the pre defined number of iterations, 200 in this case.
+# The shuffle function has been used to move the agents in a random order, rather than the order dictated by the list, adding to the robustness of the model.
+# The agents behaviour here; eat, move and share is dictated by the agentframework1 class, the code can be seen in that file.
+
 for j in range(num_of_iterations):
     random.shuffle(agents)
     for i in range(num_of_agents):
@@ -73,7 +88,9 @@ for j in range(num_of_iterations):
         agents[i].eat()
         agents[i].share_with_neighbours(neighbourhood)
   
-
+#Again I have then added to show the updated visual output following ths code.
+#The thrd visual output shows how the 15 agents have moved after 200 iterations.
+#The changing environemnt shows how the agents have taken away from the environment.
       
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
@@ -83,19 +100,15 @@ for i in range(num_of_agents):
 matplotlib.pyplot.show()
 
 
-
-
-#below is the code to make the agents add back to the environment once they have
-#taken in x amount, this is defined in the agent framework file.
-#I'm leaving this out of the final code for now.
-
-
-
+#The final piece of behavioural code is then added. This is the agents added back to the environment.
+#In the context of this project it adds realism, the sheep can only eat so much.
+#Again, the code that defines how this action takes place is in the agentframework1 agent class.
 for i in range(num_of_agents):
     agents[i].vom()
 
 
-
+#A final visual output is then created, output 4 shows how the environment looks after all interaction has taken place.
+#This generally shows the environemnt as much darker as the spread of resources is much less concentrated than initally due to the agent movement.
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.imshow(environment)
@@ -104,7 +117,8 @@ for i in range(num_of_agents):
 matplotlib.pyplot.show()
         
         
-
+#This final bit of code calculates distance between which is used in the agent interaction.
+#The final commented out code can be inluded, this shows the distance between agents.
 
 for agent0 in agents:
     for agent1 in agents:
